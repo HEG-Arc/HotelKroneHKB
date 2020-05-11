520fb561c8eee38e5820a99f6396ec8554639d8e
cedric.gaspoz <cedric.gaspoz@he-arc.ch>
2020-05-11 16:25:16 +0200

    Production settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,18 +1,21 @@
 from .base import *  # noqa
 from .base import env
 
+
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["example.com"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["hotelkrone.be"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
 DATABASES["default"] = env.db("DATABASE_URL")  # noqa F405
 DATABASES["default"]["ATOMIC_REQUESTS"] = True  # noqa F405
 DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)  # noqa F405
+
+MEDIA_URL = "https://cdn.hotelkrone.be"
 
 # CACHES
 # ------------------------------------------------------------------------------
